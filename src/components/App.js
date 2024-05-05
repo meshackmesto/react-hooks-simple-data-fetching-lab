d90cacad748ee52de886b36a9170fa7a4a892111
meshackmesto <meshackorina93@gmail.com>
2024-05-05 19:12:42 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React,{ useEffect, useState} from "react";
+
+
+function App(){
+const [isDogImage, setIsDogImage] = useState(null);
+
+
+
+useEffect(() =>{
+    fetch("https://dog.ceo/api/breeds/image/random")
+   .then((res) => res.json())
+   .then((data) => {
+    setIsDogImage(data.message);
+   });
+}, []);
+
+
+if(!isDogImage)
+    return <p>Loading...</p>
+
+    return(
+        <div>
+            <img src={isDogImage} alt="A Random Dog" />
+        </div>
+    )
+};
+
+export default App;
